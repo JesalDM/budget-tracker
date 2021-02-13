d8ff104e2e0f2688aea986426078a44794cdf829
Jesal Mehta <jesaldmehta@gmail.com>
2021-02-12 21:05:56 -0500

    Cached the files, installed sand activated the service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,84 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/assets/js/index.js',
+    '/assets/js/indexedDb.js',
+    '/assets/css/styles.css',
+    '/assets/images/icons/icon-192x192.png',
+    '/assets/images/icons/icon-512x512.png',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    './manifest.json',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
+    
+];
+  
+   
+   // 'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
+   // 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(PRECACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
